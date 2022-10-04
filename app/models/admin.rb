76f4fd392e52ab6545ab3dfcf1391b753da75e45
fujichi70 <nyq_o0@yahoo.co.jp>
2022-10-04 15:59:10 +0900

    adminCreateRegistrationFunction

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -2,5 +2,23 @@ class Admin < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable,# :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         authentication_keys: [:admin_number]
+
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if admin_number = conditions.delete(:admin_number)
+      where(conditions).where(admin_number: admin_number).first
+    else
+      where(conditions).first
+    end
+  end
+
+  # 登録時に email を不要にする
+  def email_required?
+    false
+  end
+  def email_changed?
+    false
+  end
 end
---
+++ db/migrate/20221003122238_add_admin_number_to_admins.rb
@@ -0,0 +1,7 @@
+class AddAdminNumberToAdmins < ActiveRecord::Migration[7.0]
+  def change
+    add_column :admins, :admin_number, :string, null: false, default: ""
+
+    add_index :admins, :admin_number, unique: true
+  end
+end
