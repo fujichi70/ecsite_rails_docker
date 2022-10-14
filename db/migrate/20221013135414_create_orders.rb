b45cb3b360f8b227345cc4ef1cd06aafb2fe859e
fujichi70 <nyq_o0@yahoo.co.jp>
2022-10-14 14:57:37 +0900

    create UserOrder migration model

--- db/migrate/20221013135414_create_orders.rb
+++ db/migrate/20221013135414_create_orders.rb
@@ -1,11 +1,12 @@
 class CreateOrders < ActiveRecord::Migration[7.0]
   def change
     create_table :orders do |t|
-      t.string :user_id, null: false, default: ""
+      t.integer :user_id, null: false, default: ""
       t.string :order_id, null: false, default: ""
-      t.string :delivery_request
-      t.string :delivery_fee, null: false, default: ""
-      t.string :total_fee, null: false, default: ""
+      t.integer :quantity, null: false, default: ""
+      t.date :delivery_requestday
+      t.integer :delivery_fee, null: false, default: ""
+      t.integer :total_fee, null: false, default: ""
  
       t.timestamps
     end
